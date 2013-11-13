c9b98b129835af14021543a5b01bda5b732d1a0b
hermanchanhm <hermanchanhm@gmail.com>
2013-11-12 17:27:45 -0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by Herman on 11/12/13.
+//  Copyright (c) 2013 Herman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "testAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([testAppDelegate class]));
+    }
+}
---
+++ test/testAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  testAppDelegate.h
+//  test
+//
+//  Created by Herman on 11/12/13.
+//  Copyright (c) 2013 Herman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
