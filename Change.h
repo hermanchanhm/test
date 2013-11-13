031608e9a6e586d1ab2d0f9c707f5e26594709a2
Craig Steinke <Seer@dhcp-v004-220.mobile.uci.edu>
2013-11-12 17:47:26 -0800

    Implemented change

---
+++ Change.h
@@ -0,0 +1,13 @@
+//
+//  Change.h
+//  test
+//
+//  Created by Craig Steinke on 11/12/13.
+//  Copyright (c) 2013 Herman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Change : NSObject
+
+@end
---
+++ Change.m
@@ -0,0 +1,13 @@
+//
+//  Change.m
+//  test
+//
+//  Created by Craig Steinke on 11/12/13.
+//  Copyright (c) 2013 Herman. All rights reserved.
+//
+
+#import "Change.h"
+
+@implementation Change
+
+@end
